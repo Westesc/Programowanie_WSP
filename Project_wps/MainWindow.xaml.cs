2679085e8e49e000b055ed6800400d8a386a2e24
dotBlueShoes <matthewstrumillo@gmail.com>
2023-03-08 10:07:07 +0100

    Update MainWindow.xaml.cs

--- Project_wps/MainWindow.xaml.cs
+++ Project_wps/MainWindow.xaml.cs
@@ -13,16 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Project_wps
-{
+namespace Project_wps {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
     }
+
 }
